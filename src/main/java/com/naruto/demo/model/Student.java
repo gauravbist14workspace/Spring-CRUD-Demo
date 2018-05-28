57d4c863c2608562726ecd080a302ba0c327a7fd
gaurav <gaurav.b@india.nec.com>
2018-05-28 16:31:12 +0530

    Changes 1.0.1

--- src/main/java/com/naruto/demo/model/Student.java
+++ src/main/java/com/naruto/demo/model/Student.java
@@ -12,7 +12,7 @@ public class Student {
 	@Id 
 	@Column(name="id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id1;
+	private int id;
 	
 	@Column(name="firstname")
 	private String firstname;
